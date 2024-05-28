e6a9c1d7c6dead2512d33545a599c53949eab684
Wpoundit <166181096+Wpoundit@users.noreply.github.com>
2024-05-28 12:20:33 -0700

    EXLAIM

--- finalproject/js/script.js
+++ finalproject/js/script.js
@@ -5,7 +5,7 @@ function insultMe() {
         "You are not pretty enough to be this dumb",
         "You look easy to draw",
         "Do you eat crayons?",
-        "You are as sharp as a bowling ball"
+        "You are as sharp as a bowling ball!"
     ];
     
     const randomIndex = Math.floor(Math.random() * insults.length);
