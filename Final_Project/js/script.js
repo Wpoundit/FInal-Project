f0cb9cd6bae749f2501967620cf3332c40b18907
Jianne <100251005+Inarhis@users.noreply.github.com>
2024-06-06 16:40:20 -0700

    fixed code

--- Final_Project/js/script.js
+++ Final_Project/js/script.js
@@ -1,3 +1,21 @@
+// Function to make AJAX request to fetch emoji details
+function fetchEmojiDetails() {
+    var name = 'slightly smiling face';
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/emoji?name=' + encodeURIComponent(name),
+        headers: { 'X-Api-Key': 'a+LDBpNVCBkfRtzFMlRvIA==fglvmXuhQzCY5JGi' },
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+}
+
+// Function to generate insults
 function insultMe() {
     const insults = [
         "If you were any slower you would be going backwards",
@@ -13,3 +31,8 @@ function insultMe() {
     
     document.getElementById('insultContainer').textContent = insult;
 }
+
+// Call the function to fetch emoji details when the page loads
+$(document).ready(function() {
+    fetchEmojiDetails();
+});
